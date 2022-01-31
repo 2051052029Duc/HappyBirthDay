da9b67e1e4ed3a3831c5e5a6c059b3297ed7134d
2051052029Duc <2051052029duc@ou.edu.vn>
2022-01-31 17:08:55 +0700

    demo1
    hpbd

---
+++ script.js
@@ -0,0 +1,17 @@
+const overplay = document.querySelector(".overplay")
+const avatarBtn = document.querySelector('.avatar img');
+
+avatarBtn.onclick = function() {
+    overplay.style.display = "flex"
+}
+
+overplay.onClick = () => {
+
+}
+
+window.onclick = function(e){
+    if (e.target.classList.contains('overplay')) {
+        overplay.style.display = 'none'
+    }
+}
+
